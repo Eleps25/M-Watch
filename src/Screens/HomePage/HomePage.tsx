f09185e3d7650df6bfc37181f8766857502e35ca
Jakub Mezulanik <mezuja@gmail.com>
2022-08-07 00:08:31 +0200

    using useApiFetch in HomePage to fetch data

--- src/Screens/HomePage/HomePage.tsx
+++ src/Screens/HomePage/HomePage.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+
+import useApiFetch from '../../CustomHooks/useApiFetch';
 
 import styles from './HomePage.style'
 
 const HomePage = () => {
+    const { data, isLoad } = useApiFetch('https://raw.githubusercontent.com/24i/smartapps-test/main/data.json');
+
     return (
-        <View>
+        <ScrollView>
             <Text>Home Page</Text>
-        </View>
+        </ScrollView>
     )
 }
 
