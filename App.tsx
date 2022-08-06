88e22b1b6c29b77975f2894368eed3c5dd006285
Jakub Mezulanik <mezuja@gmail.com>
2022-08-07 00:00:44 +0200

    useApiFetch custom hook

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import Application from './src/index';
 import { Text, View } from 'react-native';
+import HomePage from './src/Screens/HomePage/index';
 
 const App = () => {
   return (
     <View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Application />
+      <HomePage />
     </View>
   );
 }
--- src/CustomHooks/useApiFetch.tsx
+++ src/CustomHooks/useApiFetch.tsx
@@ -1,5 +1,23 @@
-const useApiFetch = () => {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const useApiFetch = (url: string) => {
+    const [data, setData] = useState([]);
+    const [isLoad, setLoad] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(url);
+                !!res.data && setData(res.data), setLoad(true)
+            }
+            catch (err) { console.log(err) }
+        }
+
+        fetchData()
+    }, [])
+
+    return { data, isLoad }
 }
 
 export default useApiFetch;
