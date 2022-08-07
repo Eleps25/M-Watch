bc7018a0a26f364b08bf7b6ea6f08f5f78ccc4b6
Jakub Mezulanik <mezuja@gmail.com>
2022-08-07 12:34:02 +0200

    detailpage basic logic added

--- src/Screens/DetailPage/DetailPage.style.tsx
+++ src/Screens/DetailPage/DetailPage.style.tsx
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: 200,
+        height: 250
+    }
 })
 
 export default styles;
--- src/Screens/DetailPage/DetailPage.tsx
+++ src/Screens/DetailPage/DetailPage.tsx
@@ -1,11 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 
 import styles from './DetailPage.style'
 
-const DetailPage = () => {
+interface DetailPageData {
+    id: number,
+    title: string,
+    year: string,
+    duration: string,
+    genres: string[],
+    director: string,
+    actors: string,
+    plot: string,
+    posterUrl: string
+}
+
+const DetailPage = (data: DetailPageData) => {
+    const { title, year, duration, plot, posterUrl } = data;
     return (
         <View>
-            <Text>Detail Page</Text>
+            <View>
+                <ImageBackground
+                    source={{ uri: `${posterUrl}` }}
+                    style={styles.image}
+                >
+                    <Text>{title}</Text>
+                </ImageBackground>
+            </View>
+            <View>
+                <Text>Year: {year}</Text>
+                <Text>Duration: {duration} min</Text>
+                <Text>Plot: {plot}</Text>
+            </View>
         </View>
     )
 }
